6b0764c58853028388a1aa312e75b4e6775d7a4a
celesteag <celeste.ag.2c@gmail.com>
2025-10-24 16:54:40 +0100

    second commit

---
+++ js/toogle-info.js
@@ -0,0 +1,7 @@
+function toggleInfo(card) {
+            card.classList.toggle('active'); //Property of CSS classes
+
+            setTimeout(() => {
+                card.classList.remove('active'); //We automatically remove the class
+            }, 2000);
+        }
